6707ac9ecf0bfbe6776ad9a05a7e88ad49b55045
Asim-khan972 <asimabas96@gmail.com>
2023-07-29 17:45:34 +0500

    problem-7

--- problem.ts
+++ problem.ts
@@ -1,13 +1,26 @@
 
-console.log(`problem-6`)
-// Stripping Names: Store a person’s name, and include some whitespace characters 
-// at the beginning and end of the name. Make sure you use each character combination, "
-// \t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. 
-// Then print the name after striping the white spaces.
+console.log(`problem-7`)
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. 
+// Be sure to enclose your operations in print statements to see the results.
 
 
 
 
 
-const personname = ` Albert Einstein  `  ;
-console.log(`${personname} \t \n ${personname}`);
+// Addition: 5 + 3 = 8
+const additionResult: number = 5 + 3;
+console.log("Addition Result:", additionResult);
+
+// Subtraction: 10 - 2 = 8
+const subtractionResult: number = 10 - 2;
+console.log("Subtraction Result:", subtractionResult);
+
+// Multiplication: 4 * 2 = 8
+const multiplicationResult: number = 4 * 2;
+console.log("Multiplication Result:", multiplicationResult);
+
+// Division: 16 / 2 = 8
+const divisionResult: number = 16 / 2;
+console.log("Division Result:", divisionResult);
+
+
