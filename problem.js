7233fbe499eafba290a3f86c5e1b4c54a27fb4e5
Asim-khan972 <asimabas96@gmail.com>
2023-07-29 20:26:38 +0500

    Problem-13

--- problem.js
+++ problem.js
@@ -1,5 +1,15 @@
 console.log("problem-12");
-//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same,
-//  but each message should be personalized with the person’s name.
-var names = ['Asim', 'Awais', 'Salman', 'junaid'];
-names.forEach(function (item) { return console.log("hello ".concat(item, " : how are you ")); });
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements
+//  about these items, such as “I would like to own a Honda motorcycle.”
+// Array of favorite cars
+var favoriteCars = [
+    "Tesla Model S",
+    "Porsche 911",
+    "Audi R8",
+    "BMW M3",
+    "Mercedes-Benz AMG GT",
+];
+// Print statements about each car
+favoriteCars.forEach(function (car) {
+    console.log("I would like to own a ".concat(car, "."));
+});
--- problem.ts
+++ problem.ts
@@ -1,12 +1,21 @@
 
-console.log(`problem-12`)
-//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same,
-//  but each message should be personalized with the person’s name.
+console.log(`problem-13`)
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements
+//  about these items, such as “I would like to own a Honda motorcycle.”
 
+// Array of favorite cars
+const favoriteCars: string[] = [
+  "Tesla Model S",
+  "Porsche 911",
+  "Audi R8",
+  "BMW M3",
+  "Mercedes-Benz AMG GT",
+];
 
-let names: string[] = ['Asim', 'Awais', 'Salman','junaid'];
- 
+// Print statements about each car
+favoriteCars.forEach((car) => {
+  console.log(`I would like to own a ${car}.`);
+});
 
-names.forEach(item=>console.log(`hello ${item} : how are you `));
 
 
