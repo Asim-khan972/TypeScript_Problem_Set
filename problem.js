f3a4b0ac2b3f5ddaf89b288c7f47bb40725d00d5
Asim-khan972 <asimabas96@gmail.com>
2023-07-29 20:37:43 +0500

    Problem-15

--- problem.js
+++ problem.js
@@ -1,13 +1,19 @@
 console.log("problem-14");
-// Guest List: If you could invite anyone, living or deceased, to dinner, 
-// who would you invite? Make a list that includes at least three people you’d like to invite to dinner. 
-// Then use your list to print a message to each person, inviting them to dinner.
+// Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to 
+// send out a new set of invitations. You’ll have to think of someone else to invite.
 // Array of people to invite to dinner
 var guestList = [
-    "Awais",
-    "Juniad",
+    "Mia Khalifa",
+    "Leah Gotti",
     "Nora Fatihe",
 ];
+//// the person who is not comming 
+var notCommingGuest = 'Nora Fatihe';
+/// here is new guset 
+var newGuest = 'Sunny Leone ';
+/// filter the guest who is not comming 
+guestList = guestList.filter(function (person) { return person !== notCommingGuest; });
+guestList.push(newGuest);
 // Print invitations to each person
 guestList.forEach(function (person) {
     console.log("Dear ".concat(person, ",\nYou are invited to join us for dinner. The dinner will be a delightful gathering, and we would be honored to have your presence.\nLooking forward to\n   meeting you!\n\nBest regards,\n Asim khan"));
--- problem.ts
+++ problem.ts
@@ -1,17 +1,27 @@
 
-console.log(`problem-14`)
-// Guest List: If you could invite anyone, living or deceased, to dinner, 
-// who would you invite? Make a list that includes at least three people you’d like to invite to dinner. 
-// Then use your list to print a message to each person, inviting them to dinner.
+console.log(`problem-15`)
+// Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to 
+// send out a new set of invitations. You’ll have to think of someone else to invite.
+
 
 
 
 // Array of people to invite to dinner
-const guestList: string[] = [
-  "Awais",
-  "Juniad",
+let guestList: string[] = [
+  "Mia Khalifa",
+  "Leah Gotti",
   "Nora Fatihe",
 ];
+//// the person who is not comming 
+ let notCommingGuest :string = 'Nora Fatihe';
+
+ /// here is new guset 
+ let newGuest :string = 'Sunny Leone ';
+
+   /// filter the guest who is not comming 
+  guestList = guestList.filter((person)=>person !== notCommingGuest)
+
+  guestList.push(newGuest);
 
 // Print invitations to each person
 guestList.forEach((person) => {
