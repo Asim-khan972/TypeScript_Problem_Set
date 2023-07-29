b7bbed3069da8c99eeb01147484340609900f203
Asim-khan972 <asimabas96@gmail.com>
2023-07-29 16:29:09 +0500

    Problem 2 solution

---
+++ problem.ts
@@ -0,0 +1,6 @@
+// Personal Message: Store a person’s name in a variable, and print a message to that person.
+//  Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+let PersonName = "Asim khan";
+console.log(`problem-2 `)
+console.log(`heloo ${PersonName}   would you like to learn some Python today?`)
