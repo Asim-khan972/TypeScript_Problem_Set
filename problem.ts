d47421da50501bdd2d9dc35a3e7805fb0671e608
Asim-khan972 <asimabas96@gmail.com>
2023-07-29 17:48:54 +0500

    Problem-9

--- problem.ts
+++ problem.ts
@@ -1,33 +1,15 @@
 
-console.log(`problem-8`)
-// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. 
-// Be sure to enclose your operations in print statements to see the results.
+console.log(`problem-9`)
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a 
+// message that reveals your favorite number. Print that message.
+// Store your favorite number in a variable
 
+const favoriteNumber: number = 42;
 
+// Create a message using the favorite number variable
+const message: string = `My favorite number is ${favoriteNumber}.`;
 
-// You should create four lines that look like this:
-
-
-
-// Addition: 5 + 3 = 8
-console.log(`-------------------------------------------------------`)
-const additionResult: number = 5 + 3;
-console.log("Addition Result:", additionResult);
-console.log(`-------------------------------------------------------`)
-
-// Subtraction: 10 - 2 = 8
-const subtractionResult: number = 10 - 2;
-console.log("Subtraction Result:", subtractionResult);
-console.log(`-------------------------------------------------------`)
-
-// Multiplication: 4 * 2 = 8
-const multiplicationResult: number = 4 * 2;
-console.log("Multiplication Result:", multiplicationResult);
-console.log(`-------------------------------------------------------`)
-
-// Division: 16 / 2 = 8
-const divisionResult: number = 16 / 2;
-console.log("Division Result:", divisionResult);
-console.log(`-------------------------------------------------------`)
+// Print the message
+console.log(message);
 
 
