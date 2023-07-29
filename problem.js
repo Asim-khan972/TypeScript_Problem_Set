0e5e7e16b8671ec60f86ac312beca263949b6d41
Asim-khan972 <asimabas96@gmail.com>
2023-07-29 17:36:24 +0500

    problem-5

--- problem.js
+++ problem.js
@@ -1,5 +1,6 @@
-console.log("problem-4");
-// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name 
-// of its author. Your output should look something like the following, including the quotation marks:
-// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
-console.log(" Albert Einstein once said, \u201CA person who never made a mistake never tried anything new.\u201D");
+console.log("problem-5");
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. 
+// Then compose your message and store it in a new variable called message. Print your message.
+var personname = 'Albert Einstein';
+var Quote = "A person who never made a mistake never tried anything new.";
+console.log(" ".concat(personname, " once said, \u201C").concat(Quote, "\u201D"));
--- problem.ts
+++ problem.ts
@@ -1,9 +1,10 @@
 
-console.log(`problem-4`)
-// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name 
-// of its author. Your output should look something like the following, including the quotation marks:
-
-// Albert Einstein once said, “A person who never made a mistake never tried  anything new.”
+console.log(`problem-5`)
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. 
+// Then compose your message and store it in a new variable called message. Print your message.
 
 
-console.log(` Albert Einstein once said, “A person who never made a mistake never tried anything new.”`)
+
+const personname = 'Albert Einstein';
+const Quote= "A person who never made a mistake never tried anything new.";
+console.log(` ${personname} once said, “${Quote}”`);
