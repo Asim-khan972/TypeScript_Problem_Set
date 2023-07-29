837e04bb5564149635789195e623478e2c0e8f36
Asim-khan972 <asimabas96@gmail.com>
2023-07-29 17:33:34 +0500

    problem-4

--- problem.ts
+++ problem.ts
@@ -3,7 +3,7 @@ console.log(`problem-4`)
 // Famous Quote: Find a quote from a famous person you admire. Print the quote and the name 
 // of its author. Your output should look something like the following, including the quotation marks:
 
-// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+// Albert Einstein once said, “A person who never made a mistake never tried  anything new.”
 
 
 console.log(` Albert Einstein once said, “A person who never made a mistake never tried anything new.”`)
