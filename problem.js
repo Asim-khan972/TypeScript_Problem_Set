ed936e111177f87c90fe595d9e62f18c53b4d0f4
Asim-khan972 <asimabas96@gmail.com>
2023-07-29 20:24:09 +0500

    Problem-12

--- problem.js
+++ problem.js
@@ -1,5 +1,5 @@
-console.log("problem-11");
-//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing
-// each element in the list, one at a time.
+console.log("problem-12");
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same,
+//  but each message should be personalized with the person’s name.
 var names = ['Asim', 'Awais', 'Salman', 'junaid'];
-names.forEach(function (item) { return console.log(item); });
+names.forEach(function (item) { return console.log("hello ".concat(item, " : how are you ")); });
--- problem.ts
+++ problem.ts
@@ -1,12 +1,12 @@
 
-console.log(`problem-11`)
-//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing
-// each element in the list, one at a time
+console.log(`problem-12`)
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same,
+//  but each message should be personalized with the person’s name.
 
 
 let names: string[] = ['Asim', 'Awais', 'Salman','junaid'];
  
 
-names.forEach(item=>console.log(item));
+names.forEach(item=>console.log(`hello ${item} : how are you `));
 
 
