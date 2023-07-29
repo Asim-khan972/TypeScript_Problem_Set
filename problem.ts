d29589e5c481f57f7928461baf2c67421757cc15
Asim-khan972 <asimabas96@gmail.com>
2023-07-29 20:39:39 +0500

    problem-16

--- problem.ts
+++ problem.ts
@@ -1,7 +1,7 @@
 
-console.log(`problem-15`)
-// Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to 
-// send out a new set of invitations. You’ll have to think of someone else to invite.
+console.log(`problem-16`)
+// Start with your program from Exercise 14. Add a print statement at the end of your program stating the name 
+// of the guest who can’t make it.
 
 
 
@@ -29,4 +29,4 @@ guestList.forEach((person) => {
    meeting you!\n\nBest regards,\n Asim khan`);
 });
 
-
+console.log(`Unfortunately, ${notCommingGuest} can't make it to the dinner.`);
