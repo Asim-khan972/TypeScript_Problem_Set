0dc41a34b8e4dc5ec5da23c04157b164ca8108d8
Asim-khan972 <asimabas96@gmail.com>
2023-07-29 17:56:59 +0500

    Problem-8

--- problem.ts
+++ problem.ts
@@ -1,29 +1,33 @@
 
-console.log(`problem-10`)
-//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. 
-// Then write one sentence describing what the program does.
+console.log(`problem-8`)
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. 
+// Be sure to enclose your operations in print statements to see the results.
 
 
-/*
-Author: Asim 
-Date: July 29, 2023
-Description: This TypeScript program demonstrates basic arithmetic operations and displays the result of each operation.
-*/
+
+// You should create four lines that look like this:
+
+
 
 // Addition: 5 + 3 = 8
+console.log(`-------------------------------------------------------`)
 const additionResult: number = 5 + 3;
-console.log("Addition Result:", additionResult);
+console.log("Addition Result:", additionResult );
+console.log(`-------------------------------------------------------`)
 
 // Subtraction: 10 - 2 = 8
 const subtractionResult: number = 10 - 2;
 console.log("Subtraction Result:", subtractionResult);
+console.log(`-------------------------------------------------------`)
 
 // Multiplication: 4 * 2 = 8
 const multiplicationResult: number = 4 * 2;
 console.log("Multiplication Result:", multiplicationResult);
+console.log(`-------------------------------------------------------`)
 
 // Division: 16 / 2 = 8
 const divisionResult: number = 16 / 2;
 console.log("Division Result:", divisionResult);
+console.log(`-------------------------------------------------------`)
 
 
