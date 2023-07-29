2711aa24477d36413abb394176eea7360d9bb25e
Asim-khan972 <asimabas96@gmail.com>
2023-07-29 20:22:28 +0500

    Problem-11

--- problem.js
+++ problem.js
@@ -1,20 +1,5 @@
-console.log("problem-10");
-//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. 
-// Then write one sentence describing what the program does.
-/*
-Author: Asim
-Date: July 29, 2023
-Description: This TypeScript program demonstrates basic arithmetic operations and displays the result of each operation.
-*/
-// Addition: 5 + 3 = 8
-var additionResult = 5 + 3;
-console.log("Addition Result:", additionResult);
-// Subtraction: 10 - 2 = 8
-var subtractionResult = 10 - 2;
-console.log("Subtraction Result:", subtractionResult);
-// Multiplication: 4 * 2 = 8
-var multiplicationResult = 4 * 2;
-console.log("Multiplication Result:", multiplicationResult);
-// Division: 16 / 2 = 8
-var divisionResult = 16 / 2;
-console.log("Division Result:", divisionResult);
+console.log("problem-11");
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing
+// each element in the list, one at a time.
+var names = ['Asim', 'Awais', 'Salman', 'junaid'];
+names.forEach(function (item) { return console.log(item); });
