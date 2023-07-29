8e183c4c3c3e502d3cb3c7dc0f0ec7ccffaa5d2f
Asim-khan972 <asimabas96@gmail.com>
2023-07-29 23:22:55 +0500

    Problem-21

--- problem.js
+++ problem.js
@@ -1,42 +1,15 @@
-console.log("problem-20");
-// Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
-// let car = 'subaru';
-// console.log("Is car == 'subaru'? I predict True.")
-// console.log(car == 'subaru')
-// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
-// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
-var car = 'subaru';
-var age = 25;
-var temperature = 20;
-var isRaining = true;
-var fruits = ['apple', 'banana', 'orange'];
-// Test 1: Check if the car is 'subaru'
-console.log("Is car == 'subaru'? I predict True.");
-console.log(car == 'subaru');
-// Test 2: Check if the age is greater than or equal to 18
-console.log("Is age >= 18? I predict True.");
-console.log(age >= 18);
-// Test 3: Check if the temperature is less than 30 degrees Celsius
-console.log("Is temperature < 30? I predict True.");
-console.log(temperature < 30);
-// Test 4: Check if it's raining
-console.log("Is it raining? I predict True.");
-console.log(isRaining);
-// Test 5: Check if 'apple' is in the fruits array
-console.log("Is 'apple' in the fruits array? I predict True.");
-console.log(fruits.includes('apple'));
-// Test 6: Check if the car is 'honda'
-console.log("Is car == 'honda'? I predict False.");
-console.log(car == 'honda');
-// Test 7: Check if the age is less than 21
-console.log("Is age < 21? I predict False.");
-console.log(age < 21);
-// Test 8: Check if the temperature is greater than or equal to 25 degrees Celsius
-console.log("Is temperature >= 25? I predict False.");
-console.log(temperature >= 25);
-// Test 9: Check if it's snowing
-console.log("Is it snowing? I predict False.");
-console.log(isRaining);
-// Test 10: Check if 'grape' is in the fruits array
-console.log("Is 'grape' in the fruits array? I predict False.");
-console.log(fruits.includes('grape'));
+console.log("problem-21");
+// Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+// • Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+// • Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+// Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
+var alien_color = 'green';
+if (alien_color === 'green') {
+    console.log("5 points");
+}
+else if (alien_color === 'red') {
+    console.log("10 points");
+}
+else {
+    console.log("20 points ");
+}
--- problem.ts
+++ problem.ts
@@ -1,60 +1,20 @@
 
-console.log(`problem-20`)
-// Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+console.log(`problem-21`)
+// Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+// • Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
 
-// let car = 'subaru';
+// • Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
 
-// console.log("Is car == 'subaru'? I predict True.")
+// Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
 
-// console.log(car == 'subaru')
 
-// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// Alien Colors #2
+let alien_color: string = 'red';
 
-// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
-
-let car = 'subaru';
-let age = 25;
-let temperature = 20;
-let isRaining = true;
-let fruits = ['apple', 'banana', 'orange'];
-
-// Test 1: Check if the car is 'subaru'
-console.log("Is car == 'subaru'? I predict True.");
-console.log(car == 'subaru');
-
-// Test 2: Check if the age is greater than or equal to 18
-console.log("Is age >= 18? I predict True.");
-console.log(age >= 18);
-
-// Test 3: Check if the temperature is less than 30 degrees Celsius
-console.log("Is temperature < 30? I predict True.");
-console.log(temperature < 30);
-
-// Test 4: Check if it's raining
-console.log("Is it raining? I predict True.");
-console.log(isRaining);
-
-// Test 5: Check if 'apple' is in the fruits array
-console.log("Is 'apple' in the fruits array? I predict True.");
-console.log(fruits.includes('apple'));
-
-// Test 6: Check if the car is 'honda'
-console.log("Is car == 'honda'? I predict False.");
-console.log(car == 'honda');
-
-// Test 7: Check if the age is less than 21
-console.log("Is age < 21? I predict False.");
-console.log(age < 21);
-
-// Test 8: Check if the temperature is greater than or equal to 25 degrees Celsius
-console.log("Is temperature >= 25? I predict False.");
-console.log(temperature >= 25);
-
-// Test 9: Check if it's snowing
-console.log("Is it snowing? I predict False.");
-console.log(isRaining);
-
-// Test 10: Check if 'grape' is in the fruits array
-console.log("Is 'grape' in the fruits array? I predict False.");
-console.log(fruits.includes('grape'));
-
+if (alien_color === 'green') {
+  console.log("Congratulations! You just earned 5 points for shooting down a green alien.");
+} else if (alien_color === 'yellow') {
+  console.log("Congratulations! You just earned 10 points for shooting down a yellow alien.");
+} else {
+  console.log("Congratulations! You just earned 15 points for shooting down a red alien.");
+}
